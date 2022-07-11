345be5632fd56bded3e771765d28e8cef823fa09
thinhntr <thinhntr@gmail.com>
2022-07-11 10:43:11 +0700

    Install tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,21 @@
-import Head from 'next/head'
-
+import Head from 'next/head';
 const Home = () => {
-  return (
-    <>
-      <Head>
-        <title>Point of sale</title>
-      </Head>
-    </>
-  )
-}
+    return (
+        <>
+            <Head>
+                <title>Point of sale</title>
+            </Head>
+            <h1 className="text-3xl font-bold text-red-400 underline">
+                Enter product&apos;s name
+            </h1>
+            <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+                <li>Item 4</li>
+            </ul>
+        </>
+    );
+};
 
-export default Home
+export default Home;
