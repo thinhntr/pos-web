b2ff4592baec60546f473b7be366e1c59c921593
thinhntr <thinhntr@gmail.com>
2022-07-12 16:28:04 +0700

    Add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import '../styles/global.css';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
+import '../styles/global.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <title>Point of sale</title>
+                <link
+                    rel="icon"
+                    type="image/x-icon"
+                    href="/images/favicon.png"
+                />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
