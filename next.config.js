98da0e26ef56518baffc3d6563a97f7cb70c5926
thinhntr <thinhntr@gmail.com>
2022-07-12 16:27:42 +0700

    Add lang to html

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    i18n: {
+        locales: ['vi'],
+        defaultLocale: 'vi'
+    },
     reactStrictMode: true,
     swcMinify: true
 };
